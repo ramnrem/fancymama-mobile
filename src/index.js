752377f5c41777793618e6f1c8381b54955aa015
ramnrem <30977844+ramnrem@users.noreply.github.com>
2018-06-22 03:01:46 +0300

    в исходниках связанной задачи не было шрифтов

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 /*import one from "./some.js";*/
+let currentSlide = 1;
+let currentArrslide = 1;
+let translateWidth = 0;
+let arrTranslateWidth = 0;
+let slideCounter = $('.slider-wrapper').children().length;
+let arrSlideCounter = $('.js-arrslider-wrapper').children().length / 2;
+
+//функция автоматической прокрутки слайдов
+function autoSlider() {
+	if (currentSlide == slideCounter) {
+		$('.slider-wrapper').css('transform', 'translate(0, 0)');
+		currentSlide = 1;
+	} else {
+		translateWidth = -$('.slider-container').width() * (currentSlide);
+		$('.slider-wrapper').css({
+		    'transform': 'translate(' + translateWidth + 'px, 0)'
+		});
+		currentSlide++;
+	}
+	if (currentArrslide == arrSlideCounter) {
+		$('.js-arrslider-wrapper').css('transform', 'translate(0, 0)');
+		currentArrslide = 1;
+	} else {
+		arrTranslateWidth = -$('.js-arrslider-container').width() * (currentArrslide);
+		$('.js-arrslider-wrapper').css({
+		    'transform': 'translate(' + arrTranslateWidth + 'px, 0)'
+		});
+		currentArrslide++;
+	}
+}
 
 $(document).ready(function() {
 	//Активация меню
@@ -20,4 +50,33 @@ $(document).ready(function() {
 			});
 		}
 	})
+
+	$('.left-arr').click(function(){
+		if (currentArrslide == 1) {
+			translateWidth = -$('.arrslider-container').width() * (arrSlideCounter - 1)
+			$('.arrslider-wrapper').css({'transform': 'translate(' + translateWidth + 'px, 0)'});
+			currentArrslide = arrSlideCounter;
+		} else {
+			translateWidth = -$('.arrslider-container').width() * (currentArrslide - 2);
+			$('.arrslider-wrapper').css({
+			    'transform': 'translate(' + translateWidth + 'px, 0)'
+			});
+			currentArrslide--;
+		}
+	})
+	$('.right-arr').click(function(){
+		if (currentArrslide == arrSlideCounter) {
+			$('.arrslider-wrapper').css('transform', 'translate(0, 0)');
+			currentArrslide = 1;
+		} else {
+			translateWidth = -$('.arrslider-container').width() * (currentArrslide);
+			$('.arrslider-wrapper').css({
+			    'transform': 'translate(' + translateWidth + 'px, 0)'
+			});
+			currentArrslide++;
+		}
+	})
+	
+
+	setInterval(autoSlider, 4000);
 });
